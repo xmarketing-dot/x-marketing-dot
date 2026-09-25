c69a6926d2f97057d00ae866b776c04a6cc58f8f
boystalktr <boystalktr@gmail.com>
2026-09-25 16:15:31 +0300

    new couner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -161,7 +161,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         
-        {/* Yandex Metrika Counter - Search Analytics & Heatmap Tracking */}
+        {/* Yandex Metrika Counters (112120217 & 113056145) - Search Analytics & Heatmap Tracking */}
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
@@ -171,8 +171,9 @@ export default function RootLayout({
                 m[i].l=1*new Date();
                 for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
                 k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-              })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=112120217', 'ym');
+              })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
 
+              // Counter 1 (112120217)
               ym(112120217, 'init', {
                 ssr: true,
                 webvisor: true,
@@ -180,10 +181,38 @@ export default function RootLayout({
                 ecommerce: "dataLayer",
                 referrer: document.referrer,
                 url: location.href,
+                accurateTrackBounce: true,
+                trackLinks: true
+              });
+
+              // Counter 2 (113056145)
+              ym(113056145, 'init', {
+                ssr: true,
+                webvisor: true,
+                clickmap: true,
+                ecommerce: "dataLayer",
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce: true,
+                trackLinks: true
               });
             `,
           }}
         />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/112120217"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+            <img
+              src="https://mc.yandex.ru/watch/113056145"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
 
         {/* Google AdSense */}
         <script
